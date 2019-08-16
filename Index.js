18fcf8491a349a59ebed748ff0003cfa10629ea6
autumn-ragland <raglanda998@gmail.com>
2019-08-16 14:15:35 -0500

    Graded 08-16-2019 14:13:44

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 function main() {
     // Put the start of your program in a main function. Create a class for Books with name, rating, genre, and author. Create 3 instances of Books and put them in an array. Afterwards print each one in the format below:
     // The book [BOOK NAME] has [RATING] stars!!
-    var bookArray = [{Book_Name:[], Rating:[]}];
+    var bookArray = [{Book_Name:[], Rating:[]}]; // this should be an empty array
     class Books {
         constructor(Name, Rating, Genre, Author) {
             this.name = Name;
@@ -13,7 +13,7 @@ function main() {
     let newB1 = new Books("Harry Potter", "five", "Fantasy", "JB");
     let newB2 = new Books("Cat in The Hat", "five", "Fantasy", "DS");
     let newB3 = new Books("Of Mice and Men", "four", "Drama", "MD");
-    bookArray[0].Book_Name.push(newB1.name);
+    bookArray[0].Book_Name.push(newB1.name); // you should be pushing the object as a whole into the array not in peices
     bookArray[0].Book_Name.push(newB2.name);
     bookArray[0].Book_Name.push(newB3.name);
     bookArray[0].Rating.push(newB1.rating);
@@ -24,6 +24,7 @@ function main() {
         console.log("The Book " + bookArray[0].Book_Name[i]+ " has " + bookArray[0].Rating[i] + " stars!!!")
         i++;
     }
+    // this technically works its an inefficent way to use objects 
 
 }
 main();
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # javascript-class-review1-cw
-
+1/1 correct - points off for method. See index.js for comments. Score:4 points
 ### Problem 1:
 
 Put the start of your program in a main function. Create a class for Books with name, rating, genre, and author. Create 3 instances of Books and put them in an array. Afterwards print each one in the format below:
